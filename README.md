a3b268e2a54655608668060f7772c7a6ddbc0732
HouckDev <houckdeveloper@gmail.com>
2024-09-08 15:13:29 -0500

    Updated Readme, implemented caesar decryption

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Author: Paden Houck
-Description: Submission for CSC-207 MP 1, Classical Encryption; Java classes for simple ciphers
-External Resources: None
+# Mini-Project 1: Classical Encryption
+
+Paden Houck
+
+A project for CSC-207 2024Fa.
+
+Submission for CSC-207 MP 1, Classical Encryption; Java classes for simple ciphers
+
+# CITATIONS -
+
 GitHub: https://github.com/HouckDev/csc-207-mp1
--- src/main/java/edu/grinnell/csc207/util/CipherUtils.java
+++ src/main/java/edu/grinnell/csc207/util/CipherUtils.java
@@ -8,9 +8,14 @@ public class CipherUtils {
   private static char int2letter(int i) {
     return (char) i; // convert int to ascii
   }
-  
+
+  // this could be done more cleanly, but it works
   private static char addLetters(char a, char b) {
     return int2letter((((letter2int(a) - 97) + (letter2int(b) - 97)) % 26) + 97); // add ascii codes, wrap around alphabet
+  }
+
+  private static char subtractLetters(char a, char b) {
+    return int2letter((((letter2int(a) - 97) - (letter2int(b) - 97)) % 26) + 97); // add ascii codes, wrap around alphabet
   }
 
   public static String caesarEncrypt(String str, char letter) {
@@ -22,7 +27,11 @@ public class CipherUtils {
   }
 
   public static String caesarDecrypt(String str, char letter) {
-    return str; // STUB
+    String newStr = "";
+    for (int i = 0; i < str.length(); i++) {
+      newStr = newStr + subtractLetters(str.charAt(i), letter); // mathmatically subtract current letter and key letter, add to working string
+    }
+    return newStr;
   }
 
   public static String vigenereEncrypt(String str, String key) {
