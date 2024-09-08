e5189a90a1b2bff484809cbda72b35f63695abb8
HouckDev <houckdeveloper@gmail.com>
2024-09-08 14:20:19 -0500

    Program decides what functions to use based on parameters

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -2,7 +2,7 @@ package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
 import java.util.HashMap; // import the HashMap class
-
+import edu.grinnell.csc207.util.CipherUtils;
 public class Cipher {
   public static void main(String[] args) {
     //initialize program flags
@@ -25,13 +25,34 @@ public class Cipher {
       (programFlags.containsKey("-encode") ^ programFlags.containsKey("-decode")) && // check desired result
       (programFlags.containsKey("-caesar") ^ programFlags.containsKey("-vigenere")) // check encoding method
     ) {
-      //program flags validated, begin execution of program
+      // print program flags out
       PrintWriter pen = new PrintWriter(System.out, true);
       for (int i = 0; i < args.length; i++) {
         pen.printf("args[%d] = \"%s\"\n", i, args[i]);
       }
       pen.close();
-
+      
+      // program flags validated, begin execution of program
+      PrintWriter penOutput = new PrintWriter(System.out, true);
+      if (programFlags.containsKey("-encode")) {
+        if (programFlags.containsKey("-caesar")) {
+          // encrypt text with caesar cipher
+          penOutput.printf(CipherUtils.caesarEncrypt(programFlags.get("param_stringToEncode"),programFlags.get("param_encodingKey").charAt(0)));
+        } else { // we know the '-vigenere' flag must be present
+          // encrypt text with vigenere cipher
+          penOutput.printf(CipherUtils.vigenereEncrypt(programFlags.get("param_stringToEncode"),programFlags.get("param_encodingKey")));
+        }
+      } else { // we know the '-decode' flag must be present, begin decoding
+        if (programFlags.containsKey("-caesar")) {
+          // decrypt text with caesar cipher
+          penOutput.printf(CipherUtils.caesarDecrypt(programFlags.get("param_stringToEncode"),programFlags.get("param_encodingKey").charAt(0)));
+        } else { // we know the '-vigenere' flag must be present
+          // decrypt text with vigenere cipher
+          penOutput.printf(CipherUtils.vigenereDecrypt(programFlags.get("param_stringToEncode"),programFlags.get("param_encodingKey")));
+        }
+      }
+      
+      penOutput.close();
     } else {
       System.err.println("Error: Invalid parameters");
     }
