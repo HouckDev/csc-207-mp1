01ea54ca50091c76739441eef3b44e4ce4260d6d
HouckDev <houckdeveloper@gmail.com>
2024-09-08 15:08:37 -0500

    Finish implementing caesar cipher encryption

--- src/main/java/edu/grinnell/csc207/util/CipherUtils.java
+++ src/main/java/edu/grinnell/csc207/util/CipherUtils.java
@@ -2,22 +2,23 @@ package edu.grinnell.csc207.util;
 
 public class CipherUtils {
   private static int letter2int(char letter) {
-    return letter - '0'; // return the ascii id of letter
+    return (int) letter; // return the ascii id of letter
   }
 
   private static char int2letter(int i) {
     return (char) i; // convert int to ascii
   }
+  
   private static char addLetters(char a, char b) {
-    return int2letter(((letter2int(a) + letter2int(b) - 49) % 26) + 49); // add ascii codes, wrap around alphabet
+    return int2letter((((letter2int(a) - 97) + (letter2int(b) - 97)) % 26) + 97); // add ascii codes, wrap around alphabet
   }
 
   public static String caesarEncrypt(String str, char letter) {
     String newStr = "";
     for (int i = 0; i < str.length(); i++) {
-      newStr = newStr + addLetters(str.charAt(i), letter);
+      newStr = newStr + addLetters(str.charAt(i), letter); // mathmatically add current letter and key letter, add to working string
     }
-    return newStr; // STUB
+    return newStr;
   }
 
   public static String caesarDecrypt(String str, char letter) {
